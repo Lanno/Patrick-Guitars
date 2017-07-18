37f32bc8653979597a5e61ad302144ecb8ed15c6
Nolan Luckett <nluckett@sara.com>
2017-07-18 00:01:56 -0700

    Added a bunch of images
    Filled in the contents of the 3 pages and the left sidebar

--- index.php
+++ index.php
@@ -37,20 +37,135 @@
 		</div>
 		<div id="wrapper">
 			<div id="left">
-				<dl>
-					<dt id="about"></dt>
-					<dd></dd>
+				<div id="about">
+					<img src="img/about - white.png"/>
+				
+					<script>
+						$("#about").on("click", 
+							function () {
+								$("#center div.text")
+									.css(
+										{
+											opacity: 0,
+											display: "none"
+										}
+									);
+								
+								$("#display")
+									.animate(
+										{
+											width: 400,
+											height: 400,
+											margin: 10
+										},
+										750
+									);
+									
+								$("#aboutText")
+									.css({display:"block"})	
+									.delay(750)
+									.animate(
+										{
+											opacity: 1
+										},
+										750
+									);
+							}
+						);
+					</script>
+				</div>
 
-					<dt id="build"></dt>
-					<dd></dd>
+				<div id="build">
+					<img src="img/work - white.png"/>
+				
+					<script>
+						$("#build").on("click", 
+							function () {
+								$("#center div.text")
+									.css(
+										{
+											opacity: 0,
+											display: "none"
+										}
+									);
+								
+								$("#display")
+									.animate(
+										{
+											width: 400,
+											height: 400,
+											margin: 10
+										},
+										750
+									);
+									
+								$("#buildText")
+									.css({display:"block"})
+									.delay(750)
+									.animate(
+										{
+											opacity: 1
+										},
+										750
+									);
+							}
+						);
+					</script>
+				</div>
 
-					<dt id="gallery"></dt>
-					<dd></dd>
-				</dl>
+				<div id="gallery">
+					<img src="img/gallery.png"/>
+				
+					<script>
+						$("#gallery").on("click", 
+							function () {
+								$("#center div.text")
+									.animate(
+										{
+											opacity: 0
+										}
+									);
+								
+								$("#display")
+									.delay(750)
+									.animate(
+										{
+											width: 800,
+											height: 800,
+											margin: 0
+										},
+										750
+									);
+							}
+						);
+					</script>
+				</div>
 			</div>
 
 			<div id="center">
 				<div id="display"></div>
+				
+				<div id="aboutText" class="text">
+					<img src="../img/about.png"/>
+				
+					<p>
+						Patrick's Guitars makes hand built guitars in Valley Center, CA. We specialize in electric guitars with varying degrees of acoustic charm, ranging from solid-body to hollow-body archtops with some unique semi-hollow concepts in between.
+					</p>
+					
+					<p>
+						In order to leverage the unique characteristics of our instruments, we use both traditional coil pickups, and piezoelectric transducers. Our guitars are wired so that you can create any tone that you want by blending the acoustic and electric qualities in inexhaustible combinations.
+					</p>
+				</div>
+				
+				<div id="buildText" class="text">
+					<img src="../img/build.png"/>
+				
+					<p>
+						Each guitar is hand assembled with some pieces being carved and shaped one wood shaving at a time.
+						The process of creating the archtop guitars is the most labor intensive, with each top
+						requiring multiple stages of refinement of the front and back halves of the body, with the front require special attention to both the interior and exterior.
+					</p>
+				</div>
 			</div>
 			
 			<div id="right">
